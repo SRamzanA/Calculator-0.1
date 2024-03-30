fddf572d956fa506ee25a11adabaecc6f5a3bd9e
SRamzanA <ramzansmajlov14@gmail.com>
2024-03-30 19:57:09 +0700

    добавлен градиент и улучшена работа функций

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ function displayAddFunc(fun) {
     if (Number.isInteger(displayContent[displayContent.length-1]) || Number.isFinite(displayContent[displayContent.length-1])) {
         displayContent.push(fun)
         display.textContent = displayContent.join("")
+    } else if (displayContent.length == 0) {
+        // Ничего
     } else {
         displayContent.pop()
         displayContent.push(fun)
