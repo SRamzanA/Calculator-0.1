88344544bfa5dedf17208893541cf8885006fd5f
SRamzanA <ramzansmajlov14@gmail.com>
2024-06-22 21:28:33 +0700

    Адаптировал под телефон и добавил клментарии в script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let display = document.getElementById("display")
 let numberButton = document.querySelectorAll(".b-number")
 
@@ -6,23 +5,35 @@ let displayContent = []
 display.textContent = displayContent.join("") 
 
 function deleteall() {
+    // Делаем displayContent[] пустым
     displayContent = []
     display.textContent = displayContent.join("")
 }
 function delpop() {
+    // Удаляем последний элемент
     displayContent.pop()
     display.textContent = displayContent.join("")
 }
 function displayAddNum(num) {
+    // Добавляем значение в displayContent[]
     displayContent.push(parseInt(num))
     display.textContent = displayContent.join("")
 }
 function displayAddFunc(fun) {
+    if (fun == "^") {
+        fun = "**"
+    } else if (fun == "÷") {
+        fun = "/"
+    }
+
+    // Если последний символ - число, то вставляем {fun}
     if (Number.isInteger(displayContent[displayContent.length-1]) || Number.isFinite(displayContent[displayContent.length-1])) {
         displayContent.push(fun)
         display.textContent = displayContent.join("")
+    // Если список пуст - ничего не делаем
     } else if (displayContent.length == 0) {
-        // Ничего
+        return
+    // Если последнее значение списка - знак, то удаляем его и вставляем {fun}
     } else {
         displayContent.pop()
         displayContent.push(fun)
